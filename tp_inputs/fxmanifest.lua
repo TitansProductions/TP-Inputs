d7c68839827343e1182185a47d103daa87ff4710
TitansProductions <84135181+TitansProductions@users.noreply.github.com>
2023-03-15 01:07:55 +0200

    Update fxmanifest.lua

--- tp_inputs/fxmanifest.lua
+++ tp_inputs/fxmanifest.lua
@@ -1,4 +1,5 @@
-game 'rdr3'
+games { 'rdr3', 'gta5' }
+  
 fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
