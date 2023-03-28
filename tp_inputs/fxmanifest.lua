3d45266f610a8d531625ecc63972878b342474a0
TitansProductions <84135181+TitansProductions@users.noreply.github.com>
2023-03-28 20:29:32 +0300

    Add files via upload

--- tp_inputs/fxmanifest.lua
+++ tp_inputs/fxmanifest.lua
@@ -1,12 +1,12 @@
 games { 'rdr3', 'gta5' }
-  
+
 fx_version 'adamant'
+
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-author 'Nosmakos'
-version '1.1.0'
-
 ui_page 'html/index.html'
+
+version '1.2.0'
 
 client_scripts { 'client.lua' }
 
