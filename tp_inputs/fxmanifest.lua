87881e9007471eefe0ce54e0a6831daa740179b3
TitansProductions <84135181+TitansProductions@users.noreply.github.com>
2024-03-29 11:37:26 +0200

    Update fxmanifest.lua

--- tp_inputs/fxmanifest.lua
+++ tp_inputs/fxmanifest.lua
@@ -4,13 +4,13 @@ fx_version 'adamant'
 
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-ui_page 'html/index.html'
-
+author 'Nosmakos'
+description 'Titans Productions - Input Dialogs'
 version '1.2.0'
+
+ui_page 'html/index.html'
 
 client_scripts { 'client.lua' }
 
-files {
-  'html/**/*'
-}
+files { 'html/**/*' }
 
